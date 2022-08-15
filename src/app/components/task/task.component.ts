ec5cc4b96df8af33836d284bcaa7becefe02e226
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-08-15 19:00:42 +0800

    Setup json-server and fetch data from task service

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-task',
@@ -8,10 +9,15 @@ import { TASKS } from 'src/app/mock-tasks';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) { 
+
+  }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) =>{
+      this.tasks = tasks;
+    })
   }
 
 }
